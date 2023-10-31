54851cfe19ec45e1bba9be8bf6240a1320b7b818
Hazem-Kelziye <hkelziye@outlook.com>
2023-10-31 09:29:11 +0300

    make rocket's initial x position harder

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import math
 import matplotlib.pyplot as plt
 import json
 
-EPISODES_NUMBER = 1
+EPISODES_NUMBER = 10
 SETPOINTS = [0, -1.05, 0]  # Setpoints/desired-points for optimizing the PID controller
 # ep_counter = 1
 episodes = {}
@@ -53,8 +53,8 @@ for ep_counter in range(1, EPISODES_NUMBER + 1):
         vy_data.append(observation[8])
         omega_data.append(observation[9])
 
-        # Taking actions w.r.t. the PID controller's feedback
-        # If one of the legs contacts the ground i.e. MISSION_ACCOMPLISHED==1 set action_y = 0 (kill off throttle engine)
+        # Taking actions w.r.t. the PID controller's feedback, If one of the legs contacts the ground i.e.
+        # MISSION_ACCOMPLISHED==1 set action_y = 0 (kill off throttle engine)
         action_y = np.clip(y_controller.update(
             [observation[1], observation[8]], abs(observation[0]) + SETPOINTS[1]), -1, 1)
 
@@ -66,16 +66,13 @@ for ep_counter in range(1, EPISODES_NUMBER + 1):
         # Adding the S-A pairs, the state-space's dimension is 9 and the action-space's dimension is 2
         sa_pairs.append([list(observation)[:6] + list(observation[7:]), list(action_set)[1:]])  # Adding the (s,a) pairs
 
-
         # Making a scheme for learning whether the landing was successful or not
         largest_reward = reward if reward > largest_reward else largest_reward
 
         if done:
-            print("Simulation done.")
-
             # Deciding whether the landing was successful or not
-            success = True if largest_reward >= 0.05 else False
-
+            success = True if largest_reward >= 0.1 else False
+            print(f"Simulation {ep_counter} done : {success}.")
             # Making a dictionary to store the episodic Dataset in a JSON file
             episodes[f"episode{ep_counter}"] = {
                 "SA_pairs": sa_pairs,
@@ -85,8 +82,9 @@ for ep_counter in range(1, EPISODES_NUMBER + 1):
 
     env.close()
 
+
 # json_object = json.dumps(episodes)
-# with open("episodes9.json", "w") as outfile:
+# with open("episodes.json", "w") as outfile:
 #     outfile.write(json_object)
 
 
@@ -100,10 +98,10 @@ def plot_response():
     plt.plot(omega_data, label="omega")
     plt.legend()
     plt.grid()
-    plt.ylim(-1.6, 1.6)
+    plt.ylim(-1.1, 1.1)
     plt.title('2-DoF PID Control')
     plt.ylabel('Value')
     plt.xlabel('Steps')
     plt.show()
 
-plot_response()  # Plotting the response of the system
+# plot_response()  # Plotting the response of the system
