41d019d1232fb6e8c02fbece19bfd883cf141f04
vboxuser <vboxuser@Ubuntu3.myguest.virtualbox.org>
2023-11-21 11:39:02 +0300

    remove redundancy and make code more compact

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-"""
-Created on Tue Oct 26 09:34:00 2023
-
-@author: Hazem
-"""
 # import gym
 import gym.spaces
 import numpy as np
@@ -10,30 +5,6 @@ import rocket_lander_gym
 from pidcontroller import *
 import math
 import matplotlib.pyplot as plt
-import json
-import torch
-
-SETPOINTS = [0, -1.05, 0]  # Setpoints/desired-points for optimizing the PID controller
-episodes = {}
-largest_reward = 0  # Dummy variable for checking whether we landed successfully or not
-
-resulting_pattern = []  # add the 3 state 1 action pairs
-dataset = []  # aggregate the 3s,1a pairs
-
-sa_pairs = []  # For storing the state-action pairs
-
-ACTION_X = 0  # !Setting the throttle's gimbal DoF to 0 permanently!
-action_y = -0.1
-action_theta = 0
-
-# Initialize the state-space's lists
-x_pos_data, y_pos_data, orient_data, vx_data, vy_data, omega_data = [], [], [], [], [], []
-
-# Initialization for our PID controllers with their Gains
-y_controller = PIDy(15000, 10, 5000, SETPOINTS[1])
-theta_controller = PIDtheta(1000, 2.5, 750, SETPOINTS[2])
-
-env = gym.make('RocketLander-v0')
 
 
 def discretize_actions(action):
@@ -51,6 +22,38 @@ def discretize_actions(action):
     return intervals[-1] if action >= intervals[-1] else None
 
 
+# Function for plotting the response of the system
+def plot_response(*args):
+    plt.plot(args[0], label="x")
+    plt.plot(args[1], label="y")
+    plt.plot(args[2], label="theta")
+    plt.plot(args[3], label="vx")
+    plt.plot(args[4], label="vy")
+    plt.plot(args[5], label="omega")
+    plt.legend()
+    plt.grid()
+    plt.ylim(-1.1, 1.1)
+    plt.title('2-DoF PID Control')
+    plt.ylabel('Value')
+    plt.xlabel('Steps')
+    plt.show()
+
+
+SETPOINTS = [0, -1.05, 0]  # Setpoints/desired-points for optimizing the PID controller
+largest_reward = 0  # Dummy variable for checking whether we landed successfully or not
+
+ACTION_X = 0  # !Setting the throttle's gimbal DoF to 0 permanently!
+action_y = -0.1
+action_theta = 0
+
+# Initialize the state-space's lists
+x_pos_data, y_pos_data, orient_data, vx_data, vy_data, omega_data = [], [], [], [], [], []
+
+# Initialization for our PID controllers with their Gains
+y_controller = PIDy(15000, 10, 5000, SETPOINTS[1])
+theta_controller = PIDtheta(1000, 2.5, 750, SETPOINTS[2])
+
+env = gym.make('RocketLander-v0')
 env.reset()
 
 action_set = env.action_space.sample()
@@ -59,7 +62,7 @@ while True:
     env.render()
     observation, reward, done, _ = env.step(action_set)
 
-    # Appending the state variables
+    # append state variables for plotting
     x_pos_data.append(observation[0])
     y_pos_data.append(observation[1])
     orient_data.append(observation[2])
@@ -89,21 +92,4 @@ while True:
 
 env.close()
 
-
-# Function for plotting the response of the system
-def plot_response():
-    plt.plot(x_pos_data, label="x")
-    plt.plot(y_pos_data, label="y")
-    plt.plot(orient_data, label="theta")
-    plt.plot(vx_data, label="vx")
-    plt.plot(vy_data, label="vy")
-    plt.plot(omega_data, label="omega")
-    plt.legend()
-    plt.grid()
-    plt.ylim(-1.1, 1.1)
-    plt.title('2-DoF PID Control')
-    plt.ylabel('Value')
-    plt.xlabel('Steps')
-    plt.show()
-
-# plot_response()  # Plotting the response of the system
+plot_response(x_pos_data, y_pos_data, orient_data, vx_data, vy_data, omega_data)  # Plotting the response of the system
